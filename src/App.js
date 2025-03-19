e6859e85a00a17dfcc8f62784eff9cf4280dc7d8
DevMaster921 <webdev537@gmail.com>
2025-03-19 07:39:44 -0400

    feat: set up the password validator library

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PasswordValidator from "./components/PasswordValidator";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <PasswordValidator />
+    </div>
+  );
+}
+
+export default App;
